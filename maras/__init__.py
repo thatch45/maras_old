f86afebe0c0c32113ead63828acf8a8d73b8bbe8
Thomas S Hatch <thatch45@gmail.com>
2014-10-04 17:56:23 -0600

    Bring in Codernity and chnage import paths and marshal
    We can't use marshal, it allows for unsafe remote code injection.
    I chnaged it to msgpack, because msgpack is considerably faster
    and one of the very few safe binary serialization formats
    
    Also changed cdb_environment to menv

--- maras/__init__.py
+++ maras/__init__.py
@@ -1,1 +1,4 @@
 # -*- coding: utf-8 -*-
+
+__version__ = '0.5.0'
+__license__ = 'Apache 2.0'
---
+++ maras/database_gevent.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2011-2013 Codernity (http://codernity.com)
+# Copyright 2014 Thomas S Hatch
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from gevent.lock import RLock
+from maras.env import menv
+
+menv['mode'] = 'gevent'
+menv['rlock_obj'] = RLock
+
+from maras.database_safe_shared import SafeDatabase
+
+
+class GeventDatabase(SafeDatabase):
+    pass
